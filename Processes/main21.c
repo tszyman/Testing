e1760dd0d7e74a05d7a6ad77f32e56bc72c105b3
Tomasz Szymanski <tszymans@student.42.fr>
2024-10-21 14:54:06 +0200

    start

---
+++ Processes/main21.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+/* Sending an array through pipe */
+
+int	main()
+{
+
+	return 0;
+}
+
---
+++ Processes/template.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+/* Template file for all exercises */
+
+int	main()
+{
+
+	return 0;
+}
+
