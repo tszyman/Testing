48802c24367578c997933b6406d51938a14503f6
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2024-10-21 22:41:49 +0200

    Ex 20 completed

--- Processes/main20.c
+++ Processes/main20.c
@@ -5,16 +5,25 @@
 #include <signal.h>
 #include <bits/sigaction.h>
 #include <asm-generic/signal-defs.h>
+#include <sys/wait.h>
 
 /* Communicating between processes using signals */
+int	x = 0;
 
-int main()
+void	handle_sigusr1()
+{
+	if (x == 0)
+		printf("\n[HINT] Remember that multiplication is repetitive addition.\n");
+}
+
+int	main()
 {
 	int	pid;
-	int	x;
-	struct sigaction sa;
+	struct sigaction sa = { 0 };
 
 	sa.sa_flags = SA_RESTART;
+	sa.sa_handler = &handle_sigusr1;
+	sigaction(SIGUSR1, &sa, NULL);
 	
 	pid = fork();
 	if (pid == -1)
@@ -31,9 +40,10 @@ int main()
 		printf("What is the result 3 x 5: ");
 		scanf("%d", &x);
 		if (x == 15)
-			printf("Right!");
+			printf("Right!\n");
 		else
-			printf("Wrong!");
+			printf("Wrong!\n");
 	}
+	wait(NULL);
 	return 0;
 }
