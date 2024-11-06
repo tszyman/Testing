5562a7c5efd2b060752939273964566821080930
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2024-11-06 03:33:27 +0100

    ex10 done w/ conditional variable

--- Threads/main10.c
+++ Threads/main10.c
@@ -7,6 +7,7 @@
 /* Condition variables in C */
 
 pthread_mutex_t mutex_fuel;
+pthread_cond_t cond_fuel;
 int fuel;
 
 void	*fuel_filling()
@@ -20,6 +21,7 @@ void	*fuel_filling()
 		fuel += 15;
 		printf("Filling fuel... %d\n", fuel);
 		pthread_mutex_unlock(&mutex_fuel);
+		pthread_cond_signal(&cond_fuel);
 		sleep(1);
 		i++;
 	}
@@ -28,6 +30,15 @@ void	*fuel_filling()
 void	*car()
 {
 	pthread_mutex_lock(&mutex_fuel);
+	while (fuel < 40)
+	{
+		printf("Not enough fuel. Waiting...\n");
+		pthread_cond_wait(&cond_fuel, &mutex_fuel);
+		// Equivalent to:
+		// pthread_mutex_unlock(&mutex_fuel);
+		// wait for singal on cond_fuel
+		// pthread_mutex_lock(&mutex_fuel);
+	}
 	fuel -= 40;
 	printf("Got fuel. Now left %d\n", fuel);
 	pthread_mutex_unlock(&mutex_fuel);
@@ -41,6 +52,7 @@ int	main()
 	
 	fuel = 0;
 	pthread_mutex_init(&mutex_fuel, NULL);
+	pthread_cond_init(&cond_fuel, NULL);
 	if (pthread_create(&th1, NULL, &car, NULL) != 0)
 		perror("Failed to create a thread");
 	if (pthread_create(&th2, NULL, &fuel_filling, NULL) != 0)
@@ -50,5 +62,6 @@ int	main()
 	if (pthread_join(th2, NULL) != 0)
 		perror("Failed to join thread");
 	pthread_mutex_destroy(&mutex_fuel);
+	pthread_cond_destroy(&cond_fuel);
 	return 0;
 }
