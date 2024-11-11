ed972ce7ac489a7680420b524a2361f229ad0160
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2024-11-11 01:38:17 +0100

    ex19 /wo deadlock

---
+++ Threads/main19.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include <pthread.h>
+#define THREAD_NUM 8
+
+pthread_mutex_t	mutex_fuel;
+int				fuel = 50;
+
+pthread_mutex_t	mutex_water;
+int				water = 50;
+
+/* Deadlocks in C */
+
+void	*routine()
+{
+	pthread_mutex_lock(&mutex_fuel);
+	pthread_mutex_lock(&mutex_water);
+	fuel += 50;
+	water = fuel;
+	printf("Incremented fuel to: %d set water to %d\n", fuel, water);
+	pthread_mutex_unlock(&mutex_fuel);
+	pthread_mutex_unlock(&mutex_water);
+	return 0;
+}
+
+int	main()
+{
+	pthread_t	th[THREAD_NUM];
+	int			i;
+
+	pthread_mutex_init(&mutex_fuel, NULL);
+	pthread_mutex_init(&mutex_water, NULL);
+	i = 0;
+	while (i < THREAD_NUM)
+	{
+		if (pthread_create(&th[i], NULL, &routine, NULL) != 0)
+			perror("Failed to create thread");
+		i++;
+	}
+
+	i = 0;
+	while (i < THREAD_NUM)
+	{
+		if (pthread_join(th[i], NULL) != 0)
+			perror("Failed to jion thread");
+		i++;
+	}
+	printf("Fuel: %d\n", fuel);
+	pthread_mutex_destroy(&mutex_fuel);
+	pthread_mutex_destroy(&mutex_water);
+	return 0;
+}
